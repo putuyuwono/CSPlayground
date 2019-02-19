3405af0605ad60f1c1484e82d46e7474c8e7516b
putu <putuyuwono@yahoo.com>
2019-02-19 17:48:03 +0900

    add classes to repesent data structure

---
+++ DataStructure/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace DataStructure
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Prev { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
---
+++ DataStructure/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    /// <summary>
+    /// Stack Implementation using Linked List Data Structure
+    /// </summary>
+    public class Stack<T>
+    {
+        private LinkedList<T> Data { get; set; }
+
+        public Stack()
+        {
+            Data = new LinkedList<T>();
+        }
+
+        public void Push(T value)
+        {
+            Data.AddLast(value);
+        }
+
+        public T Pop()
+        {
+            var value = Data.Tail.Value;
+            Data.RemoveLast();
+            return value;
+        }
+
+        public T Peek()
+        {
+            return Data.Tail.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return Data.Count == 0;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+
+    }
+}
