2f82d8081260bba75bf40e0f6d9ac9692601e954
putuyuwono <putuyuwono@gmail.com>
2019-03-11 20:31:56 +0900

    added bubble sort

---
+++ CSPlayground/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPlayground
+{
+    public class BubbleSort
+    {
+        public static void Sort(ref int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j < n-i; j++)
+                {
+                    int prev = arr[j - 1];
+                    int next = arr[j];
+                    if (prev > next)
+                    {
+                        int temp = prev;
+                        arr[j - 1] = next;
+                        arr[j] = prev;
+                    }
+                }
+            }
+        }
+    }
+}
