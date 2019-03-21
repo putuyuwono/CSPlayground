53ad5a7bc72540a2dc2382cde6791d0b42d2eeaf
putu <putuyuwono@yahoo.com>
2019-03-21 17:51:54 +0900

    Linked List Addition

--- CSPlayground/Program.cs
+++ CSPlayground/Program.cs
@@ -10,13 +10,84 @@ namespace CSPlayground
     {
         static void Main(string[] args)
         {
-            int[] arr = { 4, 3, 2, 1, 5, 9, 8, 0, 7, 6};
-            Console.WriteLine(string.Join(" ", arr));
-            BubbleSort.Sort(ref arr);
-            Console.WriteLine(string.Join(" ", arr));
+            TestLinkedListAdd();
+        }
 
-            var result = MergeSort.Sort(arr.ToList());
-            Console.WriteLine(string.Join(" ", result));
+        static void TestLinkedListAdd() {
+            LinkedList<int> l1 = new LinkedList<int>();
+            foreach (var item in "789")
+            {
+                int x = int.Parse(item.ToString());
+                l1.AddLast(x);
+            }
+            l1 = ReverseLL(l1);
+
+            LinkedList<int> l2 = new LinkedList<int>();
+            foreach (var item in "1")
+            {
+                int x = int.Parse(item.ToString());
+                l2.AddLast(x);
+            }
+            l2 = ReverseLL(l2);
+
+            LinkedList<int> result = AddLLNode(l1.First, l2.First);
+            Console.WriteLine("Result: " + string.Join("", result.ToArray()));
+        }
+
+        /// <summary>
+        /// Limitations:
+        /// - Inputs are reversed linked list
+        /// - For example: 152 (2->5->1)
+        /// - Reversing Linked List takes O(n) complexity
+        /// </summary>
+        /// <param name="n1">Head of LinkedList 1</param>
+        /// <param name="n2">Head of LinkedList 2</param>
+        /// <returns></returns>
+        static LinkedList<int> AddLLNode(LinkedListNode<int> n1, LinkedListNode<int> n2) {
+            LinkedList<int> result = new LinkedList<int>();
+
+            int carry = 0, sum = 0, first = 0;
+            while (n1 != null || n2 != null)
+            {
+                first++;
+                sum = carry;
+
+                if (n1 != null)
+                {
+                    sum += n1.Value;
+                    n1 = n1.Next;
+                }
+
+                if (n2 != null)
+                {
+                    sum += n2.Value;
+                    n2 = n2.Next;
+                }
+
+                carry = sum / 10;
+                sum = sum % 10;
+                
+                result.AddFirst(sum);
+            }
+
+            if (carry > 0)
+                result.AddFirst(carry);
+
+            return result;
+        }
+
+        static LinkedList<int> ReverseLL(LinkedList<int> list)
+        {
+            LinkedList<int> r = new LinkedList<int>();
+
+            LinkedListNode<int> curr = list.First;
+            while (curr != null)
+            {
+                r.AddFirst(curr.Value);
+                curr = curr.Next;
+            }
+
+            return r;
         }
     }
 }
